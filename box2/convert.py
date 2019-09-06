a53e4d841158f650678a71cd901731f7850f1286
Emilio Murta <emilio.murta@msitbrasil.com>
2019-09-05 23:50:14 -0300

    remove solution from master

--- box2/convert.py
+++
@@ -1,4 +0,0 @@
-from datetime import date
-def convert_string_to_data(str):
-    date_parts = [int(num_str) for num_str in str.split('/')]
-    return date(date_parts[2], date_parts[1], date_parts[0])
